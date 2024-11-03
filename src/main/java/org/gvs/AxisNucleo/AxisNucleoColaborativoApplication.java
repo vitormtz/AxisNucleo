95c17c4431c90406eb255301e34369a3a440217a
vitormtz <vitor.brak@hotmail.com>
2024-11-03 20:20:03 -0300

    first commit

---
+++ src/main/java/org/gvs/AxisNucleo/AxisNucleoColaborativoApplication.java
@@ -0,0 +1,13 @@
+package org.gvs.AxisNucleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisNucleoColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxisNucleoColaborativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gvs/AxisNucleo/AxisNucleoColaborativoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gvs.AxisNucleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxisNucleoColaborativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
