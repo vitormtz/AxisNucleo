3835e4a6fb98028d3416c4057b6c0ee0708e08be
vitormtz <vitor.brak@hotmail.com>
2024-11-04 10:55:57 -0300

    Configured the main class

--- src/main/java/org/gvs/AxisNucleo/AxisNucleoColaborativoApplication.java
+++ src/main/java/org/gvs/AxisNucleo/AxisNucleoColaborativoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AxisNucleoColaborativoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AxisNucleoColaborativoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AxisNucleoColaborativoApplication.class, args);
+    }
 
 }
