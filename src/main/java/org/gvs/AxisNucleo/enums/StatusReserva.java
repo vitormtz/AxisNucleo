4e07ef805bc81aec88216c1939ca026aae37e036
vitormtz <vitor.brak@hotmail.com>
2024-11-04 11:45:01 -0300

    Created enums files for reservation status, room status, and user type

---
+++ src/main/java/org/gvs/AxisNucleo/enums/StatusReserva.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package org.gvs.AxisNucleo.enums;
+
+/**
+ *
+ * @author vitor
+ */
+public enum StatusReserva {
+    PENDENTE,
+    CONFIRMADA,
+    CANCELADA,
+    CONCLUIDA
+}
